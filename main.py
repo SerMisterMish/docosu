1afa14be62c69ca644d1268e8d7e69cf63d2fb4a
Nikita Khromov <hromovn@mail.ru>
2023-12-10 01:02:30 +0300

    Exception handle

--- main.py
+++ main.py
@@ -42,15 +42,16 @@ if __name__ == "__main__":
     minute = int(today.strftime("%M"))
 
     for img in images:
-        print(img)
+
         photo = "./img/" + img
         server = vk.photos.getWallUploadServer(group_id=group_id)
-        post = requests.post(server["upload_url"], files={'photo': open(photo, 'rb')}).json()
         try:
+            post = requests.post(server["upload_url"], files={'photo': open(photo, 'rb')}).json()
             result = vk.photos.saveWallPhoto(server=post["server"], photo=post["photo"],
                                              hash=post["hash"], group_id=group_id)[0]
         except:
-            continue
+            print(f"Fail on: {img}")
+
         string = "photo" + str(result["owner_id"]) + "_" + str(result["id"])
         if 23 >= hour >= 6:
             today = datetime(year, month, day, hour, 30, 0, 0)
@@ -71,7 +72,7 @@ if __name__ == "__main__":
                     month = 1
                     year += 1
                     today = datetime(year, month, day, hour, 30, 0, 0)
-        vk.wall.post(owner_id=-210600187, attachments=string,
+        vk.wall.post(owner_id=-group_id, attachments=string,
                      from_group=1, publish_date=int(time.mktime(today.timetuple())))
         hour += 3
         os.replace(photo, "./done/" + img)
